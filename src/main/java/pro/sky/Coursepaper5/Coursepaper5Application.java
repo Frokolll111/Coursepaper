3e7f961cd9fbe8f33c03027e0c3db7e33cdba607
Frokolll111 <buther1433@gmail.com>
2023-05-03 16:37:59 +0700

    Commit

---
+++ src/main/java/pro/sky/Coursepaper5/Coursepaper5Application.java
@@ -0,0 +1,13 @@
+package pro.sky.Coursepaper5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coursepaper5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Coursepaper5Application.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/Coursepaper5/Coursepaper5ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.Coursepaper5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coursepaper5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
