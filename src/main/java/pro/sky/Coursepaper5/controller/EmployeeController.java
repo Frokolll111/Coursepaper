294ea17e452a90e2840e5a1220daacfd31e1bd8c
Frokolll111 <buther1433@gmail.com>
2023-05-11 10:50:06 +0700

    Сделал домашние задание

--- src/main/java/pro/sky/Coursepaper5/controller/EmployeeController.java
+++ src/main/java/pro/sky/Coursepaper5/controller/EmployeeController.java
@@ -22,17 +22,19 @@ public class EmployeeController {
     public Employee addEmployee(@RequestParam String firstName, @RequestParam String lastName) {
         return service.add(firstName, lastName);
     }
+
     @GetMapping("/remove")
     public Employee removeEmployee(@RequestParam String firstName, @RequestParam String lastName) {
         return service.remove(firstName, lastName);
     }
+
     @GetMapping("/find")
     public Employee findEmployee(@RequestParam String firstName, @RequestParam String lastName) {
         return service.find(firstName, lastName);
     }
 
     @GetMapping
-    public Collection<Employee> findAll () {
-        return service.findAll() ;
+    public Collection<Employee> findAll() {
+        return service.findAll();
     }
 }
